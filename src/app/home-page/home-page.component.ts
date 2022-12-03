eafabc5f873f52ecf9806c7f2ca22e39b5c71d51
bernardjk <bernardjk519@gmail.com>
2022-12-02 20:37:36 -0800

    got sound working

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -17,4 +17,66 @@ export class HomePageComponent implements OnInit {
     this.gesture = event.getPrediction();
   }
 
+  playBass(){
+    let audio = new Audio();
+    audio.src = "../assets/Bass-Drum.wav";
+    audio.load();
+    audio.play();
+  }
+  playHi(){
+    let audio = new Audio();
+    audio.src = "../assets/Hi-Tom.wav";
+    audio.load();
+    audio.play();
+  }
+  playMid(){
+    let audio = new Audio();
+    audio.src = "../assets/Mid-Tom.wav";
+    audio.load();
+    audio.play();
+  }
+  playLow(){
+    let audio = new Audio();
+    audio.src = "../assets/Low-Tom.wav";
+    audio.load();
+    audio.play();
+  }
+  playFloor(){
+    let audio = new Audio();
+    audio.src = "../assets/Floor-Tom.wav";
+    audio.load();
+    audio.play();
+  }
+  playCrash(){
+    let audio = new Audio();
+    audio.src = "../assets/Crash-Cymbal.wav";
+    audio.load();
+    audio.play();
+  }
+  playHat(){
+    let audio = new Audio();
+    audio.src = "../assets/Hi-Hat.wav";
+    audio.load();
+    audio.play();
+  }
+  playRide(){
+    let audio = new Audio();
+    audio.src = "../assets/Ride-Cymbal.wav";
+    audio.load();
+    audio.play();
+  }
+  playClap(){
+    let audio = new Audio();
+    audio.src = "../assets/Clap.wav";
+    audio.load();
+    audio.play();
+  }
+  playStick(){
+    let audio = new Audio();
+    audio.src = "../assets/Cross-Sticks.wav";
+    audio.load();
+    audio.play();
+  }
+
+
 }
